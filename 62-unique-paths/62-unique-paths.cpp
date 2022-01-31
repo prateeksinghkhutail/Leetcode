a0a948e5873a3bafdad6e8d6f3e13e6ba4793e67
Prateek Singh Khutail <42620774+prateeksinghkhutail@users.noreply.github.com>
2022-01-31 12:19:43 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (98.43%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+      int k=m+n-2;
+      int r=m-1;
+      double res=1;
+    for(int i=1;i<=r;i++)
+    {
+        res=res*(k-r+i)/i;
+    }
+      
+        return (int)res;
+    }
+    
+   
+};
