bd434b96d8ebf15ad09333cdb4155a5046abcbb5
Prateek Singh Khutail <42620774+prateeksinghkhutail@users.noreply.github.com>
2022-02-05 19:53:51 +0530

    Time: 282 ms (16.31%), Space: 13.5 MB (38.37%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -10,47 +10,28 @@
  */
 class Solution {
 public:
-    ListNode *merge(vector<ListNode*>&lists,int left,int right)
+    ListNode *merge(ListNode *l1,ListNode *l2)
     {
-        if(left==right)
-            return lists[left];
-        int k=right-left+1;
-        ListNode *head,*h1,*h2,*ptr;
-        head=h1=h2=NULL;
-        h1=merge(lists,left,left+k/2-1);
-        h2=merge(lists,left+k/2,right);
-        
-        if(!h1 and !h2)
-            return head;
-        else if(!h1)
-            return h2;
-        else if(!h2)
-            return h1;
+     
         
         
-        
-         if(!h1 || (h1 and h1->val>h2->val) )
-        {head=h2;h2=h2->next;}
-        else{
-            head=h1;h1=h1->next;
-        }
-        
-        ptr=head;
-        while(h1 or h2)
+        if(l1==NULL)
+            return l2;
+        if(l2==NULL)
+            return l1;
+        if(l1->val<l2->val)
         {
-            if(!h1)
-            {ptr->next=h2;h2=h2->next;ptr=ptr->next;}
-            else if(!h2)
-            {
-                ptr->next=h1;h1=h1->next;ptr=ptr->next;
+            l1->next=merge(l1->next,l2);
+            return l1;
+        }else{
+            l2->next=merge(l1,l2->next);
+            return l2;
             }
-            else if(h1->val<h2->val)
-            {ptr->next=h1;h1=h1->next;ptr=ptr->next;}
-            else{
-                ptr->next=h2;h2=h2->next;ptr=ptr->next;
-            }
-        }
-        return head;
+        
+        
+ 
+      
+      
     
     }
         
@@ -66,42 +47,13 @@ public:
             return NULL;
         else if(k==1)
             return lists[0];
-        ListNode *head,*h1,*h2,*ptr;
-        head=h1=h2=NULL;
-        h1=merge(lists,0,k/2);
-        if(k/2+1<=k-1)
-            h2=merge(lists,k/2+1,k-1);
-        
-        
-        if(!h1 and !h2)
-            return head;
-        else if(!h1)
-            return h2;
-        else if(!h2)
-            return h1;
-        
-        if(!h1 || (h1 and h1->val>h2->val) )
-        {head=h2;h2=h2->next;}
-        else{
-            head=h1;h1=h1->next;
-        }
-        
-        ptr=head;
-        while(h1 or h2)
-        {
-            if(!h1)
-            {ptr->next=h2;h2=h2->next;ptr=ptr->next;}
-            else if(!h2)
-            {
-                ptr->next=h1;h1=h1->next;ptr=ptr->next;
-            }
-            else if(h1->val<h2->val)
-            {ptr->next=h1;h1=h1->next;ptr=ptr->next;}
-            else{
-                ptr->next=h2;h2=h2->next;ptr=ptr->next;
-            }
-        }
-        return head;
-    
+       while(lists.size()>1)
+       {
+           lists.push_back(merge(lists[0],lists[1]));
+           lists.erase(lists.begin());
+           lists.erase(lists.begin());
+           
+       }
+        return lists.front();
     }
 };
