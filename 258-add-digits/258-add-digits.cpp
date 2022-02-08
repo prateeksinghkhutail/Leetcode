594cc737c2df6637e10909d9291ccb4fdbd5dd56
Prateek Singh Khutail <42620774+prateeksinghkhutail@users.noreply.github.com>
2022-02-08 12:50:55 +0530

    Time: 8 ms (8.74%), Space: 6 MB (25.37%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -4,19 +4,8 @@ public:
         int sum;
         if(num==0)
             return 0;
-        if(num/10==0)
-            return num; 
-        while(num/10!=0)
-        {
-            int n=num;
-            sum=0;
-            while(n>0)
-            {
-                sum=sum+n%10;
-                n=n/10;
-            }
-            num=sum;
-        }
-        return sum;
+        else if(num%9==0) return 9;
+        else return num%9;
+       
     }
 };
