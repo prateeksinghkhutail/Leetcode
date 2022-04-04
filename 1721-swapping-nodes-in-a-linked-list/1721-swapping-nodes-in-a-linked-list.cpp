3658503935936166e243a93836b9b194f4c48c16
Prateek Singh Khutail <42620774+prateeksinghkhutail@users.noreply.github.com>
2022-04-04 23:09:36 +0530

    Time: 1322 ms (5.07%), Space: 180.2 MB (57.49%) - LeetHub

---
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapNodes(ListNode* head, int k) {
+        ListNode* dummy=new ListNode(0,head);
+        ListNode* prev1=dummy;
+        ListNode* prev2=dummy;
+        ListNode* current=dummy;;
+      
+        
+       
+        for(int i=0;i<k;i++)
+        {
+            
+            current=current->next;
+        }
+         for(int i=0;i<k-1;i++)
+        {
+            prev1=prev1->next;
+        }
+        
+        while(current->next!=NULL)
+        {
+            prev2=prev2->next;
+            current=current->next;
+        }
+        
+        if(prev1->next==prev2)
+        {
+            current=prev2->next;
+            prev1->next->next=prev2->next->next;
+            current->next=prev1->next;
+            prev1->next=current;
+            return dummy->next;
+        }
+        
+        
+        
+          current = prev1->next;
+        // attaching n2 right after prev1
+        prev1->next = prev2->next;
+        // since prev1 is no longer needed, we will use it to store next2 = n2->next
+        prev1 = prev2->next->next;
+        // finishing splicing n2 in place
+        prev2->next->next = current->next;
+        // adding n1 after prev2
+        prev2->next = current;
+        // connecting n2->next riht after n1
+        current->next = prev1;
+        return dummy->next;
+        
+        
+    }
+};
