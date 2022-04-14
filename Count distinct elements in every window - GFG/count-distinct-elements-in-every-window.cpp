84514a915b753eb4cdf984a021d61a962357d1fe
Prateek Singh Khutail <42620774+prateeksinghkhutail@users.noreply.github.com>
2022-04-14 23:24:36 +0530

    Added solution - LeetHub

--- Count distinct elements in every window - GFG/count-distinct-elements-in-every-window.cpp
+++ Count distinct elements in every window - GFG/count-distinct-elements-in-every-window.cpp
@@ -11,45 +11,49 @@ class Solution{
     vector <int> countDistinct (int A[], int n, int k)
     {
         vector<int>v;
-        unordered_map<int,int>map;
         int distinct=0;
+        unordered_map<int,int>map;
+        
+        int ac;
+        int re=0;
         for(int i=0;i<k;i++)
         {
             int cv=A[i];
             if(map.find(cv)!=map.end())
-            {
                 map[cv]+=1;
-                
-            }
-            else{
-                map[cv]=1;
-                distinct+=1;
+            else 
+            {
+                distinct++;
+                map[cv]++;
             }
         }
         v.push_back(distinct);
-        int release=0;
-        for(int acquire=k;acquire<n;acquire++){
-            int de=A[release];
-            release+=1;
+        
+        
+        for(ac=k;ac<n;ac++)
+        {
+            int de=A[re];
+            re+=1;
             map[de]-=1;
-            if(map[de]==0){
+            if(map[de]==0)
+            {
                 map.erase(de);
-                distinct=distinct-=1;
+                distinct--;
+            }
+            int cv=A[ac];
+            if(map.find(cv)!=map.end())
+                map[cv]+=1;
+            else 
+            {
+                distinct++;
+                map[cv]++;
             }
             
-            int cv=A[acquire];
-            if(map.find(cv)!=map.end())
-            {
-                map[cv]+=1;
-                
-            }
-            else{
-                map[cv]=1;
-                distinct+=1;
-            }
             v.push_back(distinct);
+                
         }
         return v;
+        
     }
 };
 
