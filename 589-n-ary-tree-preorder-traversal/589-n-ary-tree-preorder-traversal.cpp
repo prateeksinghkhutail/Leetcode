c18bb0db67c7ee3a8155960166e4843b84662435
Prateek Singh Khutail <42620774+prateeksinghkhutail@users.noreply.github.com>
2022-04-24 11:01:38 +0530

    Time: 23 ms (77.57%), Space: 11.3 MB (50.99%) - LeetHub

---
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.cpp
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> preorder(Node* root) {
+        vector<int>v;
+        traversal(root,v);
+        return v;
+    }
+    
+    void traversal(Node* root,vector<int>&v)
+    {
+        if(!root)
+            return;
+        v.push_back(root->val);
+        for(Node* cn:root->children)
+        {
+            traversal(cn,v);
+        }
+        return;
+    }
+};
