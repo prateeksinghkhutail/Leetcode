4f90413337ed4f03efc376756c4d50c269270d1f
Prateek Singh Khutail <42620774+prateeksinghkhutail@users.noreply.github.com>
2022-05-26 13:18:45 +0530

    Time: 3 ms (54.92%), Space: 5.9 MB (79.17%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -10,3 +10,15 @@ public:
         return cnt;
     }
 };
+
+
+/*These are Builtin functions of GCC compiler
+
+__builtin_popcount(x): This function is used to count the number of one’s(set bits) in an integer.
+
+__builtin_parity(x): This function is used to check the parity of a number. This function returns true(1) if the number has odd parity else it returns false(0) for even parity.
+
+__builtin_clz(x): This function is used to count the leading zeros of the integer. Note: clz = count leading zero’s.
+
+__builtin_ctz(x): This function is used to count the trailing zeros of the given integer. Note: ctz = count trailing zeros.
+*/
