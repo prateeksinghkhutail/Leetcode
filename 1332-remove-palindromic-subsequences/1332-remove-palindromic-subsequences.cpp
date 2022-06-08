b33da4d809e7182d1e0883b0c9e89a94b081df28
Prateek Singh Khutail <42620774+prateeksinghkhutail@users.noreply.github.com>
2022-06-08 13:00:59 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (26.79%) - LeetHub

---
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        string t=s;
+        reverse(s.begin(),s.end());
+        if(s==t)
+          return 1;
+        return 2;
+    }
+};
