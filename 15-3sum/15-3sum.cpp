d0b88572ca7b0528087e6459674f3761d4be58e9
Prateek Singh Khutail <42620774+prateeksinghkhutail@users.noreply.github.com>
2022-06-20 17:17:51 +0530

    Time: 107 ms (72.33%), Space: 23.6 MB (26.07%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,42 +1,49 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>>v;
+        
+        vector<vector<int>>ans;
+        
         int n=nums.size();
+        
         sort(nums.begin(),nums.end());
+        
         for(int i=0;i<n-2;i++)
         {
-            if(i==0||(i>0 && nums[i]!=nums[i-1]))
+            if(i==0||(i>0 and nums[i-1]!=nums[i]))
             {
-            int target=0-nums[i];
-            int j=i+1;
-            int k=n-1;
-            while(j<k)
-            {
-                if(nums[j]+nums[k]==target)
-                {
-                    vector<int>t;
-                    t.push_back(nums[i]);
-                    t.push_back(nums[j]);
-                    t.push_back(nums[k]);
-                    
-                    v.push_back(t);
-                    while(j<k && nums[j]==nums[j+1])j++;
-                    while(j<k && nums[k]==nums[k-1])k--;
-                    j++;
-                    k--;
-                }
-                else if(nums[j]+nums[k]<target)
-                {
-                    j++;
-
-                }
-                else{k--;}
-            }
+                int target=0-nums[i];
+                int first=i+1;
+                int last=n-1;
             
+                while(first<last)
+                {
+                    int sum=nums[first]+nums[last];
+                    
+                    if(sum>target)
+                        last--;
+                
+                    else if(sum<target)
+                        first++;
+                
+                    else
+                    {
+                        vector<int>v;
+                        v.push_back(nums[i]);
+                        v.push_back(nums[first]);
+                        v.push_back(nums[last]);
+                        ans.push_back(v);
+                        while(first<last and nums[first+1]==nums[first])first++;
+                        while(first<last and nums[last-1]==nums[last])last--;
+                        first++;
+                        last--;
+                    
+                    
+                     }
+                 }
+                   
             }
-        
         }
-        return v;
-        }
+        return ans;
+    }
 };
