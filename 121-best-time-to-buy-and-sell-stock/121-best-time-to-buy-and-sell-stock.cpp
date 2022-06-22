4c825116c7101e0a53abc9fc63753c85051cb25b
Prateek Singh Khutail <42620774+prateeksinghkhutail@users.noreply.github.com>
2022-06-22 16:46:03 +0530

    Time: 161 ms (70.45%), Space: 93.4 MB (12.17%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,37 +1,22 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int dp[100001][2][2];
-        memset(dp,-1,sizeof(dp));
-        return maxprofit(prices,0,1,1,dp);
-    }
-    int maxprofit(vector<int>& prices,int ci,int canbuy,int k,int dp[][2][2])
-    {
-        if(ci>=prices.size()||k==0)
+        int left=0;
+        int right=1;
+        int maxp=0,gmaxp=0;
+        while(right<prices.size())
         {
-            return 0;
+            if(prices[right]>prices[left])
+            {
+                maxp=prices[right]-prices[left];
+                gmaxp=max(gmaxp,maxp);
+            }
+            else{
+                left=right;
+            }
+            right++;
         }
-        
-        if(dp[ci][canbuy][k]!=-1)
-            return dp[ci][canbuy][k];
-        
-        int ans=0;
-        if(canbuy==1)
-        {   
-            int idle=maxprofit(prices,ci+1,canbuy,k,dp);
-            int buy=-prices[ci]+maxprofit(prices,ci+1,0,k,dp);
-            ans=max(idle,buy);
-        }
-        else{
-            
-          
-            int idle=maxprofit(prices,ci+1,canbuy,k,dp);
-            int sell=prices[ci]+maxprofit(prices,ci+1,1,k-1,dp);
-            ans=max(idle,sell);
-        
-        }
-        dp[ci][canbuy][k]=ans;
-        return dp[ci][canbuy][k];
+        return gmaxp;
         
     }
 };
