b8f1846efeb990bd5a2aceabeaa191b947c88321
Prateek Singh Khutail <42620774+prateeksinghkhutail@users.noreply.github.com>
2022-08-01 09:05:41 +0530

    Time: 11 ms (7.14%), Space: 9.1 MB (26.54%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-      int k=m+n-2;
-      int r=m-1;
-      double res=1;
-    for(int i=1;i<=r;i++)
-    {
-        res=res*(k-r+i)/i;
-    }
-      
-        return (int)res;
+        unordered_map<string,int>map;
+        return solve(m,n,0,0,map);
     }
     
-   
+    int solve(int m, int n,int cr, int cl,unordered_map<string,int>&map)
+    {
+        if(cr==m-1 && cl==n-1)
+            return 1;
+        if(cr>=m || cl>=n)
+            return 0;
+        string ck=to_string(cr)+"-"+to_string(cl);
+        if(map.find(ck)!=map.end())
+            return map[ck];
+        
+        int right=solve(m,n,cr,cl+1,map);
+        int down=solve(m,n,cr+1,cl,map);
+        return map[ck]=right+down;
+        
+    }
 };
